4ba4c13df72ce59549176b85a23e56829312a051
Daniel Wedul <dwedul@sofi.org>
2019-06-12 03:44:32 -0600

    Create an abstract class EasyOverrider that contains the toString hashCode and equals functions, but requires implementation of getParamList(). I'm not yet sold on doing it this way since you can only extend using one class, but mabye it'll work out alright.

---
+++ src/main/java/EasyOverrider/EasyOverrider.java
@@ -0,0 +1,125 @@
+package EasyOverrider;
+
+/**
+ * An abstract class that overrides the equals, hashCode and toString methods using a ParamList.<br>
+ *
+ * The extender is required to implement the {@link #getParamList()} method.<br>
+ *
+ * Example:
+ * <pre>{@code
+ *
+ * import static EasyOverrider.ParamMethodRestriction.INCLUDED_IN_TOSTRING_ONLY;
+ *
+ * import Bar;
+ * import EasyOverrider.EasyOverrider;
+ * import EasyOverrider.ParamList;
+ *
+ * public class Foo extends EasyOverrider<Foo> {
+ *     private int id;
+ *     private String name;
+ *     private Bar bar;
+ *
+ *     private static ParamList<Foo> paramList = null;
+ *
+ *     static paramList<Foo> getFooParamList() {
+ *         if (paramList == null) {
+ *             paramList = ParamList.forClass(Foo.class)
+ *                                  .withParam("id", Foo::getId, INCLUDED_IN_TOSTRING_ONLY, Integer.class)
+ *                                  .withParam("name", Foo::getName, String.class)
+ *                                  .withParam("bar", Foo::getBar, Bar.class)
+ *                                  .andThatsIt();
+ *         }
+ *         return paramList;
+ *     }
+ *
+ *     &#64;Override
+ *     paramList<Foo> getParamList() {
+ *         return getFooParamList();
+ *     }
+ *
+ *     public Foo(int id, String name, Bar bar) {
+ *         this.id = id;
+ *         this.name = name;
+ *         this.bar = bar;
+ *     }
+ *
+ *     public int getId() {
+ *         return id;
+ *     }
+ *
+ *     public int getName() {
+ *         return name;
+ *     }
+ *
+ *     public int getBar() {
+ *         return bar;
+ *     }
+ * }
+ * }</pre>
+ *
+ * @param <B>  the type of object being extended.
+ * @see EasyOverriderPreventingRecursiveToString
+ */
+public abstract class EasyOverrider<B> {
+
+    /**
+     * A method that returns this class's ParamList.<br>
+     *
+     * It's best practice to do something like this:
+     * <pre>
+     * {@code
+     *
+     * private static ParamList<Foo> paramList = null;
+     * static paramList<Foo> getFooParamList() {
+     *     if (paramList == null) {
+     *         paramList = ParamList.forClass(Foo.class)
+     *                              .withParam("id", Foo::getId, INCLUDED_IN_TOSTRING_ONLY)
+     *                              .withParam("name", Foo::getName)
+     *                              .withParam("bar", Foo::getBar)
+     *                              .andThatsIt();
+     *     }
+     *     return paramList;
+     * }
+     *
+     *@literal @Override
+     * ParamList<Foo> getParamList() {
+     *     return getFooParamList();
+     * }
+     * }
+     * </pre>
+     *
+     * @return A {@link ParamList} for the extending class.
+     */
+    abstract ParamList<B> getParamList();
+
+    /**
+     * An EasyOverrider version of equals(Object).
+     *
+     * @param obj  the object to compare this object to
+     * @return True if the provided object equals this object as defined by the {@link ParamList}. False otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return getParamList().equals((B)this, obj);
+    }
+
+    /**
+     * An EasyOverrider version of hashCode().
+     *
+     * @return The hash code int for this object as defined by the {@link ParamList}.
+     */
+    @Override
+    public int hashCode() {
+        return getParamList().hashCode((B)this);
+    }
+
+    /**
+     * An EasyOverrider version of toString().
+     *
+     * @return The string representation of this object as defined by the {@link ParamList}.
+     */
+    @Override
+    public String toString() {
+        return getParamList().toString((B)this);
+    }
+}
