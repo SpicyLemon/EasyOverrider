c73a2f3a56021235c0955940aa1c88fec29e1941
Daniel Wedul <dwedul@sofi.org>
2019-07-03 04:22:32 -0600

    Clean up the testSetterGetter method.

--- src/test/java/EasyOverrider/TestingUtils/Helpers.java
+++ src/test/java/EasyOverrider/TestingUtils/Helpers.java
@@ -13,20 +13,23 @@ public class Helpers {
     }
 
     /**
-     * Tests a setter and getter for an object.
-     * I.e., it calls the setter with the given value, then asserts that the getter returns that same value.
-     * @param paramDescription - The message that will go in the assert so that you can know which parameter test failed.
-     * @param obj - The object with the getter and setter that you want to test.
-     * @param value - The value you want to use to test
-     * @param setter - The setter to use
-     * @param getter - The getter to use
-     * @param <O> - The type of object you're testing
-     * @param <P> - The type of the parameter (and value) you're testing
+     * Simple test for a setter and getter for an object.<br>
+     *
+     * It calls the setter with the given value, then asserts that the getter returns that same value.<br>
+     *
+     * @param paramDescription  the message that will go in the assert so that you can know which parameter test failed
+     * @param obj  the object with the getter and setter that you want to test - assumed not null
+     * @param value  the value you want to use to test
+     * @param setter  the setter to use - assumed not null
+     * @param getter  the getter to use - assumed not null
+     * @param <O>  the type of object you're testing
+     * @param <P>  the type of the parameter (value) you're testing
      */
-    public static <O, P> void testGetterSetter(String paramDescription, O obj, P value,
+    public static <O, P> void testSetterGetter(String paramDescription, O obj, P value,
                                                BiConsumer<O, P> setter,
                                                Function<O, P> getter) {
         setter.accept(obj, value);
-        assertEquals(paramDescription, value, getter.apply(obj));
+        P actual = getter.apply(obj);
+        assertEquals(paramDescription, value, actual);
     }
 }
