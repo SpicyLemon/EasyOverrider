f8dc84bdbe8654285ed8c3a09058aba199b4a460
Daniel Wedul <dwedul@sofi.org>
2019-07-05 04:35:09 -0600

    Add note about testing copyOf for the service.

--- src/test/java/EasyOverrider/TestParamListServiceImpl.java
+++ src/test/java/EasyOverrider/TestParamListServiceImpl.java
@@ -5,7 +5,8 @@ public class TestParamListServiceImpl {
     //TODO: Finish writing tests.
 
     //Things to test:
-    // ParamListServiceImpl(), ParamListServiceImpl(ParamListConfig), ParamListServiceImpl(ParamListService),
+    // ParamListServiceImpl(), ParamListServiceImpl(ParamListConfig),
+    // ParamListServiceImpl(ParamListService), copyOf(),
     // getConfig(), setConfig(ParamListConfig),
     // equals(Object, Object, ParamList), hashCode(Object, ParamList),
     // toString(Object, ParamList, Map), primaryToString(Object, ParamList)
